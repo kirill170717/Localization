dc90c062798110a7cea1a1ac59ce07f3a6721511
Rinnedander <kirill_170717@list.ru>
2022-01-25 16:07:11 +0700

    Библиотека для заполнения готова

--- Assets/Resources/Scripts/JsonEdit.cs
+++ Assets/Resources/Scripts/JsonEdit.cs
@@ -1,20 +1,45 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using UnityEditor;
 
 public class JsonEdit : MonoBehaviour
 {
-    public string data;
-
-    private LangEdit langEdit;
-
+    private Dictionary<string, string> texts;
+    public LangEdit langEdit;
+    public void Outputing()
+    {
+        TextAsset textAsset = Resources.Load<TextAsset>(langEdit.language);
+        if (textAsset != null)
+            langEdit.jsonText = textAsset.text;
+        else
+            langEdit.jsonText = null;
+    }
     public void Loading()
     {
-        Debug.Log(langEdit.key);
-        Debug.Log(langEdit.text);
-        data = JsonUtility.ToJson(langEdit);
-        Debug.Log(data);
+        TextAsset textAsset = Resources.Load<TextAsset>(langEdit.language);
+        if (textAsset != null)
+        {
+            texts = JsonConvert.DeserializeObject<Dictionary<string, string>>(textAsset.text);
+            texts.Add(langEdit.key, langEdit.text);
+            string json = JsonConvert.SerializeObject(texts, Formatting.Indented);
+            File.WriteAllText(Application.dataPath + "/Resources/" + langEdit.language + ".json", json);
+            AssetDatabase.Refresh();
+            Outputing();
+        }
     }
     public void Deleting()
     {
-
+        TextAsset textAsset = Resources.Load<TextAsset>(langEdit.language);
+        if (textAsset != null)
+        {
+            texts = JsonConvert.DeserializeObject<Dictionary<string, string>>(textAsset.text);
+            texts.Remove(langEdit.key);
+            string json = JsonConvert.SerializeObject(texts, Formatting.Indented);
+            File.WriteAllText(Application.dataPath + "/Resources/" + langEdit.language + ".json", json);
+            AssetDatabase.Refresh();
+            Outputing();
+        }
     }
 }
--- Assets/Resources/Scripts/LangEdit.cs
+++ Assets/Resources/Scripts/LangEdit.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEditor;
 
@@ -6,10 +5,19 @@ using UnityEditor;
 public class LangEdit : ScriptableObject
 {
     [SerializeField]
+    public string language;
+    [SerializeField]
     public string key;
     [SerializeField]
     public string text;
 
+    [TextArea(minLines: 5, maxLines: 10)]
+    public string jsonText;
+
+    public string Language
+    {
+        get { return language; }
+    }
     public string Key
     {
         get { return key; }
@@ -20,13 +28,26 @@ public class LangEdit : ScriptableObject
     }
 
     private JsonEdit jsonEdit;
+    public void Output()
+    {
+        jsonEdit = GameObject.FindWithTag("JsonEdit").GetComponent<JsonEdit>();
+        jsonEdit.Outputing();
+    }
     public void Load()
     {
-        jsonEdit.Loading();
+        if(key != "" && text != "")
+        {
+            jsonEdit = GameObject.FindWithTag("JsonEdit").GetComponent<JsonEdit>();
+            jsonEdit.Loading();
+        }
     }
     public void Delete()
     {
-        jsonEdit.Deleting();
+        if (key != "")
+        {
+            jsonEdit = GameObject.FindWithTag("JsonEdit").GetComponent<JsonEdit>();
+            jsonEdit.Deleting();
+        }
     }
 }
 
@@ -37,6 +58,11 @@ public class EditGUI : Editor
     {
         DrawDefaultInspector();
         LangEdit edit = (LangEdit)target;
+        if (GUILayout.Button("Выбрать"))
+        {
+            edit.Output();
+        }
+        GUILayout.BeginHorizontal();
         if (GUILayout.Button("Добавить"))
         {
             edit.Load();
@@ -45,5 +71,11 @@ public class EditGUI : Editor
         {
             edit.Delete();
         }
+        GUILayout.EndHorizontal();
+        GUILayout.Label("Введите название нужного языка в поле 'Language'\n" +
+            "и нажмите 'Выбрать', чтобы выбрать язык локализации.\n" +
+            "Чтобы добавить перевод, введите ключ в поле 'Key'\n" +
+            "и перевод в поле 'Text', чтобы удалить - введите нужный ключ\n" +
+            "и нажмите 'Удалить'");
     }
 }
